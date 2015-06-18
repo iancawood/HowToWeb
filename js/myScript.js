5dbb5d1128c017fc846be5d097619f58cc9cb982
Ian Wood <iwood@uwo.ca>
2015-06-17 23:47:44 -0400

    first commit

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+    console.log("This will run when all elements have been loaded and the page is ready.");
+
+    $(".select-me").text("This text was added using jQuery."); // this is a standard jQuery select.
+
+    $(".center div").html("<h4>BROOOOO!!!! HOW??!!?!?</h4>"); // you can also do cool shit like this
+});
---
+++ myScript.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function() {
+    console.log("This will run when all elements have been loaded and the page is ready.");
+
+    $(".select-me").text("This text was added using jQuery.");
+});
