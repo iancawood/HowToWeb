6000260292090f8bf3562071d81b25c6b38e461e
Ian Wood <iwood@uwo.ca>
2015-07-05 15:16:42 -0400

    add carousel

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,12 @@
-
-
 $(document).ready(function() {
-    console.log("This will run when all elements have been loaded and the page is ready.");
+	console.log("This will run when all elements have been loaded and the page is ready.");
 
-    $(".select-me").text("This text was added using jQuery."); // this is a standard jQuery select.
+	$(".select-me").text("This text was added using jQuery."); // this is a standard jQuery select.
 
-    $(".center div").html("<h4>BROOOOO!!!! HOW??!!?!?</h4>"); // you can also do cool shit like this
+	$(".center div").html("<h4>BROOOOO!!!! HOW??!!?!?</h4>"); // you can also do cool shit like this
+
+	$('.my-carousel').owlCarousel({
+		loop:true,
+		margin:10
+	})
 });
